dbb7201d6cc64aed2519f192dc3cea38473db419
MWasile <maaciej.wasilewski@gmail.com>
2023-06-30 17:54:19 +0200

    Updated visual elements and typography in the second step of registration

--- mobile/src/components/Register/RegisterDetails.tsx
+++ mobile/src/components/Register/RegisterDetails.tsx
@@ -1,10 +1,15 @@
-import {SafeAreaView} from "react-native";
-import {Box, Button, FormControl, Input, ScrollView, Text, VStack} from "native-base";
+import {Box, Button, FormControl, Input, ScrollView, Spacer, Text, VStack} from "native-base";
 import Surface from "../Surface/Surface";
 import LogoBarBig from "../../LogoBar/LogoBarBig";
 import {CalcPercentageWidth} from "../../helpers/sizing";
+import {NativeStackScreenProps} from "@react-navigation/native-stack";
+import {RootStackParamList} from "../../../App";
 
-function RegisterDetails() {
+type NavigationProps = NativeStackScreenProps<RootStackParamList, 'RegisterDetails' | 'RegisterEmail'>;
+
+function RegisterDetails({navigation, route}: NavigationProps) {
+    const userEmail = route.params?.userEmail;
+
     return (
         <Surface>
             <ScrollView>
@@ -14,29 +19,42 @@ function RegisterDetails() {
                     alignItems={"center"}
                 >
                     <VStack
-                        space={8}
-                        mt={12}
+                        space={4}
                         width={CalcPercentageWidth(80)}
                     >
-                        <Input
-                            placeholder="Username"
-                            size={'xl'}
-                        ></Input>
-                        <Input
-                            placeholder="Email"
-                            size={'xl'}
-                        ></Input>
-                        <Input
-                            type={"password"}
-                            placeholder="Password"
-                            size={'xl'}
-                        ></Input>
-                        <Input
-                            type={"password"}
-                            placeholder="Repeat password"
-                            size={'xl'}
-                        ></Input>
+                        <Text
+                            pt={4}
+                            fontSize={12}
+                        >
+                            Konto zostało utworzone przy pomocy adresu {userEmail} <Spacer />
+                            Utwórz swoją nazwę i hasło.
+                        </Text>
+                        <Box>
+                            <FormControl.Label _text={{fontSize: 12}}>
+                                Name</FormControl.Label>
+                            <Input
+                                placeholder="Username"
+                                size={'xl'}
+                            ></Input>
+                        </Box>
+                        <Box>
+                            <FormControl.Label _text={{fontSize: 12}}>
+                                Password</FormControl.Label>
+                            <Input
+                                placeholder="Password"
+                                size={'xl'}
+                            ></Input>
+                        </Box>
+                        <Box>
+                            <FormControl.Label _text={{fontSize: 12}}>
+                                Repeat password</FormControl.Label>
+                            <Input
+                                placeholder="Repeat password"
+                                size={'xl'}
+                            ></Input>
+                        </Box>
                         <Button
+                            mt={4}
                             variant={"primary"}
                             width={CalcPercentageWidth(60)}
                             alignSelf={"center"}
